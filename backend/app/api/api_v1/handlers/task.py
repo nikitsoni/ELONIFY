3e49f22f4a0eb884f30a6a16f01e56adec9b97bd
Rino <85957627+rinojames007@users.noreply.github.com>
2023-10-07 03:56:12 +0530

    Add task routes
    Signed-off-by: Rino <85957627+rinojames007@users.noreply.github.com>

--- backend/app/api/api_v1/handlers/task.py
+++ backend/app/api/api_v1/handlers/task.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends
-from app.schemas.task_schema import TaskCreate, TodoOut
+from fastapi import APIRouter, Depends, Body
+from fastapi.encoders import jsonable_encoder
+from app.schemas.task_schema import TaskCreate, TodoOut, TodoUpdate
 from app.models.user_model import User
 from app.api.deps.user_deps import get_current_user
 from app.models.task_model import Task
@@ -11,9 +12,13 @@ task_router = APIRouter()
 async def get_tasks(data: TodoOut, current_user: User = Depends(get_current_user))
 
 @task_router.post('/create')
-async def create_tasks(data: TaskCreate, current_user: User = Depends(get_current_user)):
-    return await TaskService.create_task(current_user, data)
+async def create_tasks(data: TaskCreate, response_model = TaskCreate, current_user: User = Depends(get_current_user)):
+    return await TaskService.create_task(current_user, req.data)
 
 @task_router.delete('/delete/{task_id}')
-async def delete_task(data, current_user: User = Depends(get_current_user))
+async def delete_task(data, current_user: User = Depends(get_current_user)):
     return await TaskService.delete_task(current_user, data.task_id)
+
+@task.task_router.put('/update/{task_id}')
+async def update_task(data: TodoUpdate, req: TodoUpdate = Body(...), current_user: User = Depends(get_current_user)):
+    return await TaskService.update_task(current_user, req.data)
