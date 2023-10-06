5fdefc4f239261b5726c0b642a56d192691995d5
Rino <85957627+rinojames007@users.noreply.github.com>
2023-10-07 03:38:34 +0530

    task routes

--- backend/app/api/api_v1/handlers/task.py
+++ backend/app/api/api_v1/handlers/task.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.schemas.task_schema import TaskCreate
+from app.schemas.task_schema import TaskCreate, TodoOut
 from app.models.user_model import User
 from app.api.deps.user_deps import get_current_user
 from app.models.task_model import Task
@@ -7,6 +7,13 @@ from app.services.task_service import TaskService
 
 task_router = APIRouter()
 
+@task_router.get('/')
+async def get_tasks(data: TodoOut, current_user: User = Depends(get_current_user))
+
 @task_router.post('/create')
 async def create_tasks(data: TaskCreate, current_user: User = Depends(get_current_user)):
     return await TaskService.create_task(current_user, data)
+
+@task_router.delete('/delete/{task_id}')
+async def delete_task(data, current_user: User = Depends(get_current_user))
+    return await TaskService.delete_task(current_user, data.task_id)
