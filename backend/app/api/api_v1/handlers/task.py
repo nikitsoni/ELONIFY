fb8589c57fbca50eda34991d81486f65959f029a
Nikit Soni <nikitsoni17@gmail.com>
2023-10-07 13:44:48 +0530

    Completed Task Routes

--- backend/app/api/api_v1/handlers/task.py
+++ backend/app/api/api_v1/handlers/task.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Body
 from fastapi.encoders import jsonable_encoder
-from app.schemas.task_schema import TaskCreate, TodoOut, TodoUpdate
+from typing import List
+from uuid import UUID
+from app.schemas.task_schema import TaskCreate, TaskOut, TaskUpdate
 from app.models.user_model import User
 from app.api.deps.user_deps import get_current_user
 from app.models.task_model import Task
@@ -8,17 +10,24 @@ from app.services.task_service import TaskService
 
 task_router = APIRouter()
 
-@task_router.get('/')
-async def get_tasks(data: TodoOut, current_user: User = Depends(get_current_user))
+@task_router.get('/', response_model=List[TaskOut])
+async def get_tasks(current_user: User = Depends(get_current_user)):
+    return await TaskService.list_task(current_user)
 
-@task_router.post('/create')
-async def create_tasks(data: TaskCreate, response_model = TaskCreate, current_user: User = Depends(get_current_user)):
-    return await TaskService.create_task(current_user, req.data)
+@task_router.get('/{task_id}')
+async def get_single_task(task_id: UUID, current_user: User = Depends(get_current_user)):
+    task = await TaskService.retrieve_todo(current_user, task_id)
+    return task
 
-@task_router.delete('/delete/{task_id}')
-async def delete_task(data, current_user: User = Depends(get_current_user)):
-    return await TaskService.delete_task(current_user, data.task_id)
+@task_router.post('/create', response_model = Task)
+async def create_tasks(data: TaskCreate, current_user: User = Depends(get_current_user)):
+    return await TaskService.create_task(current_user, data)
 
-@task.task_router.put('/update/{task_id}')
-async def update_task(data: TodoUpdate, req: TodoUpdate = Body(...), current_user: User = Depends(get_current_user)):
-    return await TaskService.update_task(current_user, req.data)
+@task_router.put('/{task_id}', response_model=TaskOut)
+async def update_task(task_id: UUID, data: TaskUpdate, current_user: User = Depends(get_current_user)):
+    return await TaskService.update_task(current_user, task_id, data)
+
+@task_router.delete('/{task_id}')
+async def delete_task(task_id: UUID, current_user: User = Depends(get_current_user)):
+    await TaskService.delete_task(current_user, task_id)
+    return None
--- backend/app/services/task_service.py
+++ backend/app/services/task_service.py
@@ -2,7 +2,7 @@ from typing import List
 from uuid import UUID
 from app.models.user_model import User
 from app.models.task_model import Task
-from app.schemas.task_schema import TaskCreate, TodoUpdate, TodoOut
+from app.schemas.task_schema import TaskCreate, TaskUpdate, TaskOut
 
 class TaskService:
     @staticmethod
@@ -11,19 +11,25 @@ class TaskService:
         return await task.insert()
 
     @staticmethod
-    async def delete_task(user: User, task_id: int):
-        try:
-            await Task.find_one(Task.task_id == task_id).delete()
-        except Exception as e:
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return {
-                "error": e
-            }
+    async def delete_task(user: User, task_id: UUID) -> None:
+        task = await TaskService.retrieve_todo(user, task_id)
+        if task:
+            await task.delete()
+        return None
 
     @staticmethod
-    async def update_task(user: User, data: TodoUpdate) -> Task:
-        return await Task.find_one(Task.task_id == task_id).update(Task)
+    async def update_task(user: User, task_id: UUID, data: TaskUpdate) -> Task:
+        task = await TaskService.retrieve_todo(user, task_id)
+        await task.update({"$set": data.dict(exclude_unset=True)})
+        
+        await task.save()
+        return task
 
     @staticmethod
-    async def read_task(user: User, data TodoOut) -> Task:
-        return await Task.find_one(Task.task_id == task_id)
+    async def list_task(user: User) -> List[Task]:
+        return await Task.find(Task.owner.id == user.id).to_list()
+
+    @staticmethod
+    async def retrieve_todo(user: User, task_id: UUID):
+        task = await Task.find_one(Task.task_id == task_id, Task.owner.id == user.id)
+        return task
